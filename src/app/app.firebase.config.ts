9980a6f9c30810d942fb18289a1cd4a515801a07
Krish1212 <mailkimu@gmail.com>
2017-10-04 21:13:33 +0530

    initial setup completed pages & views created navigation to pages done(need to change once authentication added) TODO: #1 - login authentication as provider #2 - adding profile page #3 - logout and home page revamp

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG =  {
+    apiKey: "AIzaSyA1vaP2bO5RBrv84npxIPEV2rKYPbgIhtI",
+    authDomain: "blogmeapp-54153.firebaseapp.com",
+    databaseURL: "https://blogmeapp-54153.firebaseio.com",
+    projectId: "blogmeapp-54153",
+    storageBucket: "blogmeapp-54153.appspot.com",
+    messagingSenderId: "642804826479"
+};
---
+++ src/pages/register/register.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage({
+  name: 'register'
+})
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+  register() {
+    this.navCtrl.push('login');
+  }
+}
